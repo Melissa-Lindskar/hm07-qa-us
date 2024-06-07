6b9d1e562c75de7fa1df18da7fffafcc00d20bc1
TripleTen Student Melissa <melissa.tau85@gmail.com>
2024-06-07 07:44:55 -0400

    Final correction of delete file

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-4ce8f5cc-7379-48ff-8a71-d343898916e1.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-dfd58172-ab93-4722-96d3-45c0b87e01b7.containerhub.tripleten-services.com"
 }
